3342329a55d67a3037946ab79b8915c155270aad
Pirmin Kalberer <pka@sourcepole.ch>
2014-01-03 19:04:02 +0100

    Postgis chapter links

--- lib/docs/filters/postgis/clean_html.rb
+++ lib/docs/filters/postgis/clean_html.rb
@@ -2,8 +2,35 @@ module Docs
   class Postgis
     class CleanHtmlFilter < Filter
       def call
+        #Remove head content
+        css('meta').remove
+        css('link').remove
+
+        #Convert title to h1
+        css('title').each do |node|
+          node.name = 'h1'
+        end
+        css('div.chapter > div > div > div > h2').remove
+
+        # Remove title 'Name' and append chapter link
+        css('div.refnamediv > h2').each do |node|
+          if node.content == 'Name' && descr = node.parent.at_css('> p')
+            descr.content = descr.content.sub(/^\w+ — /, '')
+
+            chapterref = Nokogiri::XML::Node.new 'div', doc
+            chapterref['class'] = 'chapterref'
+            link = Nokogiri::XML::Node.new 'a', doc
+            link['href'] = result[:linkup]
+            link.content = result[:chapter]
+            chapterref.add_child(link)
+            node.parent.add_next_sibling(chapterref)
+
+            node.remove
+          end
+        end
+
         # Set id attributes on <h2> instead of an empty <a>
-        css('h2.title').each do |node|
+        css('h2.title,h3.title').each do |node|
           node['id'] = node.at_css('a')['name']
         end
         #Remove TOC of chapters
--- lib/docs/filters/postgis/clean_nav.rb
+++ lib/docs/filters/postgis/clean_nav.rb
@@ -12,6 +12,9 @@ module Docs
         if node = at_css('div.navheader th[width="60%"]')
           result[:chapter] = node.content.sub(/^[\d.]+ /, '')
         end
+        if node = at_css('a[accesskey="u"]')
+          result[:linkup] = node['href']
+        end
       end
     end
   end
