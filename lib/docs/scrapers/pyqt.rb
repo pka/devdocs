4147a4e547eb01c1e77a45c0b569111310f97281
Pirmin Kalberer <pka@sourcepole.ch>
2018-03-20 10:52:09 +0100

    Update pyqt to 4.11.4 and add major version to pyqt and qgispyapi

--- lib/docs/scrapers/pyqt.rb
+++ lib/docs/scrapers/pyqt.rb
@@ -2,10 +2,6 @@ module Docs
   class Pyqt < FileScraper
     self.name = 'PyQt'
     self.type = 'pyqt'
-    self.version = '4.10.14'
-    #apt-get install python-qt4-doc
-    self.dir = '/usr/share/doc/python-qt4-doc/html'
-    self.base_url = 'http://pyqt.sourceforge.net/Docs/PyQt4'
     self.root_path = 'classes.html'
 
     html_filters.push 'pyqt/clean_nav', 'pyqt/clean_html', 'pyqt/entries'
@@ -13,9 +9,24 @@ module Docs
     options[:only_patterns] = [/^\/q/]
     options[:skip] = %w(/qml.html /qvariant.html /qsettings.html)
 
-    options[:attribution] = <<-HTML
-      Copyright &copy; Riverbank Computing Ltd and Nokia 2012<br>
-      PyQt4 is released under the terms of the GNU General Public License.
-    HTML
+    version '4' do
+      self.release = '4.11.4'
+      #apt-get install python-qt4-doc
+      self.dir = '/usr/share/doc/python-qt4-doc/html'
+      self.base_url = 'http://pyqt.sourceforge.net/Docs/PyQt4'
+      options[:attribution] = <<-HTML
+        Copyright &copy; Riverbank Computing Ltd and Nokia 2012<br>
+        PyQt4 is released under the terms of the GNU General Public License.
+      HTML
+    end
+
+=begin
+    version '5' do
+      self.release = '5.7'
+      #apt-get install pyqt5-doc
+      self.dir = '/usr/share/doc/pyqt5-doc/html'
+      self.base_url = 'http://pyqt.sourceforge.net/Docs/PyQt5'
+    end
+=end
   end
 end
--- lib/docs/scrapers/qgispyapi.rb
+++ lib/docs/scrapers/qgispyapi.rb
@@ -3,20 +3,23 @@ module Docs
     self.name = 'QGIS Python API'
     self.slug = 'qgispyapi'
     self.type = 'qgispyapi'
-    self.version = '2.8'
-    self.dir = '/home/builder/geoapis/docs/qgispyapi'
     self.root_path = 'classes.html'
 
     html_filters.push 'qgispyapi/entries'
-    options[:fix_urls] = ->(url) do
-      pyqturl = url.sub(%r{^http://pyqt.sourceforge.net/Docs/PyQt4/([\w-]+).html}, '/pyqt/\1')
-      if pyqturl != url then pyqturl else nil end
-    end
 
     options[:skip] = []
 
     options[:attribution] = <<-HTML
       QGIS is released under the terms of the GNU General Public License.
     HTML
+
+    version '2' do
+      self.release = '2.8'
+      self.dir = '/home/builder/geoapis/docs/qgispyapi'
+      options[:fix_urls] = ->(url) do
+        pyqturl = url.sub(%r{^http://pyqt.sourceforge.net/Docs/PyQt4/([\w-]+).html}, '/pyqt~4/\1')
+        if pyqturl != url then pyqturl else nil end
+      end
+    end
   end
 end
